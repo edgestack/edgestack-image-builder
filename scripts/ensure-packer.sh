f2f709aa95bea788ca20e53cbfca0fc24dd01a87
Jian Li <pyguni@gmail.com>
2021-08-20 02:20:12 -0400

    Fix: ensure to install the latest packer binary

--- scripts/ensure-packer.sh
+++ scripts/ensure-packer.sh
@@ -9,11 +9,7 @@ yum install wget zip unzip -y
 yum install epel-release -y
 yum install qemu kvm qemu-kvm libvirt virt-install bridge-utils qemu-system-x86 -y
 
-if command -v packer >/dev/null 2>&1; then exit 0; fi
-
-mkdir -p .bin && cd .bin
+rm -rf /usr/sbin/packer
 
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 yum -y install packer
-
-cd .. && rm -rf .bin
