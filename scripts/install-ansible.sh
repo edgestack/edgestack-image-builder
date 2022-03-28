a7dacc4584e28b161b8df4b1924c6fdf0167a2ed
Jian Li <pyguni@gmail.com>
2022-03-28 12:07:24 -0400

    Update the checksum for ubuntu distro, use ansible instead of ansible-local

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -5,10 +5,8 @@ os_distro="${os_distro_raw%\"}"
 os_distro="${os_distro#\"}"
 
 if [ $os_distro == "centos" ]; then
-    yum install -y epel-release
-    yum install -y ansible
-    #curl https://rpmfind.net/linux/epel/7/x86_64/Packages/a/ansible-2.9.24-2.el7.noarch.rpm -o ansible-2.9.24-2.el7.noarch.rpm
-    #yum localinstall -y ansible-2.9.24-2.el7.noarch.rpm
+    yum install centos-release-ansible-29 -y
+    yum install ansible -y    
 elif [ $os_distro == "ubuntu" ]; then
     apt-add-repository ppa:ansible/ansible
     apt-get -y update
--- scripts/install-rt-kernel.sh
+++ scripts/install-rt-kernel.sh
@@ -8,8 +8,9 @@ if [ $os_distro == "centos" ]; then
     curl -o /etc/yum.repos.d/CentOS-rt.repo https://raw.githubusercontent.com/sonaproject/edgestack-image-builder/master/ansible/roles/rt/files/CentOS-rt.repo
     yum install -y kernel-rt rt-tests tuned-profiles-realtime
 elif [ $os_distro == "ubuntu" ]; then
-    add-apt-repository ppa:damentz/liquorix && sudo apt-get update
-    apt-get install linux-image-liquorix-amd64 linux-headers-liquorix-amd64
+    echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
+    wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
+    sudo apt update -y && sudo apt install linux-xanmod -y
 elif [ $os_distro == "rocky" ]; then
     yum-config-manager --enable rt
     yum install -y kernel-rt rt-tests tuned-profiles-realtime
